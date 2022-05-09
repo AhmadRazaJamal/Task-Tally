e33a585fb357fa54f2c8c222fd38d34312f9c6ac
AhmadRazaJamal <ahmad.raza@me.com>
2022-05-09 12:14:48 -0700

    added header component and style

---
+++ src/ components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+
+const Header = () => {
+  return (
+    <Typography variant="h3" gutterBottom component="div">Task Tally</Typography>
+  )
+}
+
+export default Header
