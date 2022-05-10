e2273e5acff68967cff84833391bbf7749248414
AhmadRazaJamal <ahmad.raza@me.com>
2022-05-10 11:06:55 -0700

    passed onToggle

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import {useState} from 'react';
 function App() {
   const [tasks, setTasks] = useState([])
 
+  // Delete a task
+  const deleteTasks = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {
+        tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTasks} onToggle={toggleReminder}/> : "No Tasks to Show"
+      }
     </div>
   );
 }
