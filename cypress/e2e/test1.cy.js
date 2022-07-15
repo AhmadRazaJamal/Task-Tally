208b3d958214306b2cc10cb54b8c6842e467db0c
AhmadRazaJamal <ahmad.raza@me.com>
2022-07-14 22:22:30 -0600

    added a test and added chrome as browser

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,5 +1,8 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('My First Test', () => {
+    it('finds the content "type"', () => {
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type').click()
+        cy.url().should('include', '/commands/actions')
+    })
 })
